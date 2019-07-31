b3bd3d29b96192270012cabaa3f3964468f277a9
Chris McCormick <chris@mccormick.cx>
2019-07-31 20:24:11 +0800

    Initial commit.

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from wsgidav.wsgidav_app import WsgiDAVApp
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger("wsgidav")
+logger.propagate = True
+logger.setLevel(logging.DEBUG)
+
+passwords = {p.split(":")[0]: {"password": p.split(":")[1]} for p in os.environ.get("PASSWORDS").split(" ")}
+
+config = {
+    "provider_mapping": {
+      "/": "/home/piku/webdav/",
+    },
+    "verbose": 1,
+    "http_authenticator": {
+        "domain_controller": None,
+        "accept_basic": True,
+        "accept_digest": True,
+        "default_to_digest": True,
+    },
+    "simple_dc": {
+        "user_mapping": {
+            "*": passwords,
+            "/public": True,
+        }
+    }
+}
+
+app = WsgiDAVApp(config)
